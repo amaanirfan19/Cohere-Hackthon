ef5993e1d29de1352c4d85446adee1853f53352d
Amaan <amaanirfan19@gmail.com>
2022-08-21 12:11:07 -0400

    test

--- prompt-generator/src/App.js
+++ prompt-generator/src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, List } from "@mui/material";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 const App = () => {
   const [result, setResult] = useState([]);
-
+  const [inputresult, setInputResult] = useState([]);
+  const temp = [
+    " A chameleon with locations",
+    "Chameleon in a lab with a computer",
+    "A chicken with a helicopter on a hill",
+  ];
   const test = (v) => {
     console.log("test" + v);
   };
@@ -29,7 +34,7 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="banner-container">
         <div className="banner-text-container">
           <h1>Generate drawing prompts with ImagINE Art</h1>
@@ -46,28 +51,55 @@ const App = () => {
             placeholder="e.g Mona Lisa crying"
             InputProps={{ style: { fontSize: 24 } }}
             InputLabelProps={{ style: { fontSize: 30 } }}
+            onChange={(e) => {
+              setInputResult(e.target.value);
+            }}
           />
         </div>
-        <Button>Generate prompt</Button>
+        <Button onClick={() => generatePrompt(inputresult)}>
+          Generate prompt
+        </Button>
         <div className="category-container">
           <p className="category-title">Choose a Category!</p>
           <div className="categories">
-            <a className="cardHolder category-link" href="#/" onClick={() => generatePrompt("locations")}>
+            <a
+              className="cardHolder category-link"
+              href="#/"
+              onClick={() => generatePrompt("locations")}
+            >
               <div className="category-card category1"></div>
               <p className="category-name">Scenery</p>
             </a>
-            <a className="cardHolder category-link" href="#/">
+            <a
+              className="cardHolder category-link"
+              href="#/"
+              onClick={() => generatePrompt("people")}
+            >
               <div className="category-card category2"></div>
               <p className="category-name">Characters</p>
             </a>
-            <a className="cardHolder category-link" href="#/">
+            <a
+              className="cardHolder category-link"
+              href="#/"
+              onClick={() => generatePrompt("animals")}
+            >
               <div className="category-card category3"></div>
               <p className="category-name">Animals</p>
             </a>
           </div>
         </div>
+        <div className="prompts">
+          <ul>
+            {result.length > 0
+              ? result.map((d) => (
+                  <li key={d}>
+                    <p className="listitems">{d}</p>
+                  </li>
+                ))
+              : null}
+          </ul>
+        </div>
       </div>
-      <Footer />
     </div>
   );
 };

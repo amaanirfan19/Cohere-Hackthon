38adb945653670755e9ddb859a6fd3761dd0d9d8
Amaan <amaanirfan19@gmail.com>
2022-08-21 10:14:36 -0400

    added category feature

--- prompt-generator/src/App.js
+++ prompt-generator/src/App.js
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
-import { TextField, Container, Button } from "@mui/material";
-import "./Reset.css"
+import { TextField, Button } from "@mui/material";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 const App = () => {
   const [result, setResult] = useState([]);
 
+  const test = (v) => {
+    console.log("test" + v);
+  };
+
   const generatePrompt = (category_type) => {
+    console.log("called!");
     fetch("/generate", {
-      // Adding method type
       method: "POST",
       mode: "cors",
-
-      // Adding body or contents to send
       body: category_type,
-
-      // Adding headers to the request
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
     }).then((response) =>
       response.json().then((data) => {
         console.log(data);
-        setResult(data.text);
+        setResult(data.prompts);
       })
     );
   };
@@ -36,38 +35,39 @@ const App = () => {
           <h1>Generate drawing prompts with ImagINE Art</h1>
         </div>
       </div>
-     
+
       <div className="prompt-container">
-        <p className="prompt-title">Enter your prompt below or select a category</p>
-         <div className="prompt-feild">
+        <p className="prompt-title">
+          Enter your prompt below or select a category
+        </p>
+        <div className="prompt-feild">
           <TextField
             id="input"
             placeholder="e.g Mona Lisa crying"
             InputProps={{ style: { fontSize: 24 } }}
-            InputLabelProps={{ style: { fontSize: 30 } }} />
-         </div>
-         <Button>Generate prompt</Button>
-          {/* <button onClick={generatePrompt}> Generate Prompt</button> */}
-          {result.length > 0 ? result : null}
-          <div className="category-container">
-            <p className="category-title">Categories</p>
-            <div className="categories">
-              <a className="cardHolder category-link" href="#" onClick={generatePrompt('locations')}>
-                <div className="category-card category1"></div>
-                <p className="category-name">Scenes</p>
-              </a>
-              <a className="cardHolder category-link" href="#">
+            InputLabelProps={{ style: { fontSize: 30 } }}
+          />
+        </div>
+        <Button>Generate prompt</Button>
+        <div className="category-container">
+          <p className="category-title">Choose a Category!</p>
+          <div className="categories">
+            <a className="cardHolder category-link" href="#/" onClick={() => generatePrompt("locations")}>
+              <div className="category-card category1"></div>
+              <p className="category-name">Scenery</p>
+            </a>
+            <a className="cardHolder category-link" href="#/">
               <div className="category-card category2"></div>
-                <p className="category-name">Characters</p>
-              </a>
-              <a className="cardHolder category-link" href="#">
-                <div className="category-card category3"></div>
-                <p className="category-name">Animals</p>
-              </a>
-            </div>
+              <p className="category-name">Characters</p>
+            </a>
+            <a className="cardHolder category-link" href="#/">
+              <div className="category-card category3"></div>
+              <p className="category-name">Animals</p>
+            </a>
           </div>
         </div>
-        <Footer />
+      </div>
+      <Footer />
     </div>
   );
 };
